18e98ee72e9fdc87db8c47f1bae896dac579f092
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-19 20:17:40 +0300

    there is a bug about main.css

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const path = require("path");
 const indexRoutes = require("./routes/indexRoutes");
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(indexRoutes);
 const server = app.listen(8800, (err) => {

a7e23e116f135d0d1ec3e40469ae7d6fbaae2eda
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-20 12:28:12 +0300

    divided routes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express(); 
 const ejs = require("ejs");
 const path = require("path");
 const indexRoutes = require("./routes/indexRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(indexRoutes);
+app.use(adminRoutes);
 const server = app.listen(8800, (err) => {
   if (err) {
     const getPort = server.address().port;
